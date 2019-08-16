721145072bedad889f2eeb04026b3b0bf7626885
wilker lira <nascimento_lira@yahoo.com.br>
2019-08-15 23:25:02 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/lira/workshopmongo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.lira.workshopmongo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lira.workshopmongo.domain.User;
+import com.lira.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
