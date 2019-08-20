986738db8932e51eefb999dc81859e2b62b03c90
wilker lira <nascimento_lira@yahoo.com.br>
2019-08-20 20:38:22 -0300

    Consulta simples com @Query

--- src/main/java/com/lira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lira/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,19 @@ package com.lira.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lira.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	// ?0, pega o parâmetro do método, se tiver mais parâmetro segue sequência ?1,
+	// ?2 etc.
+	// i, ignora maiúscula e minuscúla
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
