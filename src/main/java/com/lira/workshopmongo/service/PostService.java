badc4e662cb21cc395d7222c7881dfc51c754fa0
wilker lira <nascimento_lira@yahoo.com.br>
2019-08-20 17:58:46 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lira/workshopmongo/service/PostService.java
@@ -0,0 +1,22 @@
+package com.lira.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lira.workshopmongo.domain.Post;
+import com.lira.workshopmongo.repository.PostRepository;
+import com.lira.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
